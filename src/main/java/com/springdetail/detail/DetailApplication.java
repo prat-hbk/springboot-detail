f84e5aa35399f8712fa465ace29571031526d3b5
prat-hbk <sprateek63@gmail.com>
2021-12-29 23:18:18 +0530

    Usage of Component annotation for Learning purpose

--- src/main/java/com/springdetail/detail/DetailApplication.java
+++ src/main/java/com/springdetail/detail/DetailApplication.java
@@ -1,13 +1,17 @@
 package com.springdetail.detail;
 
+import com.springdetail.detail.sub1.SpringConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DetailApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DetailApplication.class, args);
+		ConfigurableApplicationContext applicationContext= SpringApplication.run(DetailApplication.class, args);
+		SpringConfig component = applicationContext.getBean(SpringConfig.class);
+		System.out.println(component.message());
 	}
 
 }
---
+++ src/main/java/com/springdetail/detail/sub1/SpringConfig.java
@@ -0,0 +1,10 @@
+package com.springdetail.detail.sub1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringConfig {
+    public String message(){
+        return "Message from Spring Component Class";
+    }
+}
